4c3df5e84ae0e1e5971726c1523843c4f6f0272c
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-20 19:55:40 -0500

    added JAWSDB_URL to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
-connection.connect((err) => {
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Take!2over",
+        database: "burgers_db"
+    });
+}
+
+connection.connect(function(err) {
     if(err) {
         console.error('error connecting: ' + err.stack);
         return;
