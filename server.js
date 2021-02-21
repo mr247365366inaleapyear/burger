451e67835e2bbf633fbd584952c1d3fbaedc6939
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-21 11:21:37 -0500

    fixing autocorrect errors

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-import express, { static } from "express";
-import { urlencoded, json } from "body-parser";
-
+var express = require("express");
+var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(static("public"));
+app.use(express.static("public"));
 
-app.use(urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(json());
-
-import exphbs from "express-handlebars";
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-import routes from "./controllers/burgersController.js";
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
 });
